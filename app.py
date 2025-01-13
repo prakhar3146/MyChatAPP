7e121ba7995e34dc00e0e6801293e86fc4a052d8
prakhar3146 <mr.prakhar@gmail.com>
2025-01-13 20:28:29 +0530

    post=80

--- app.py
+++ app.py
@@ -228,4 +228,6 @@ def monitor_clients():
 socketio.start_background_task(monitor_clients)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=False, host='0.0.0.0', port=8000)
+    socketio.run(app, debug=False, host='0.0.0.0', port=80)
+
+
